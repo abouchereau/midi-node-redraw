30399531f0f1066ba8513a93c5c52a2721e1ce18
anto <anthonybouchereau@hotmail.com>
2022-04-01 14:02:37 +0200

    midi

--- index.js
+++ index.js
@@ -3,7 +3,13 @@
 const MidiOutput = require("./lib/MidiOutput");
 const MidiInput = require("./lib/MidiInput");
 const MidiRemap = require("./lib/MidiRemap");
+const Presets = require("./lib/Presets");
 const yargs = require('yargs');
+
+const NOTE_OFF = 8;
+const NOTE_ON = 9;
+const CC = 11;
+const PC = 12;
 
 const argv = yargs
     .option("midiOutputName", {
@@ -18,18 +24,13 @@ const argv = yargs
     }).argv;
 
 
+
+
+
 if (typeof argv.midiOutputName != "string" || typeof argv.midiInputName != "string") {
     console.log("Please Select MIDI Input and Output in parameters.")
     process.exit();
 }
-
-
-
-let midiRemap1 = new MidiRemap((cmd, channel, param1, param2) => {
-    return [[16 * cmd + channel, param1, param2]];
-});
-
-
 
 
 let midiOut = new MidiOutput();
@@ -42,11 +43,12 @@ midiOut.scanDevices().then(()=> {
     })
 })
 
+let remap = Presets.changeNothing;
 
 function start() {//return same as input
     midiIn.onMidiMessage((cmd, channel, param1, param2) => {
         console.log("On Midi Message", cmd, channel, param1, param2);
-        let outMessages = midiRemap1.getMidiMessages(cmd, channel, param1, param2);
+        let outMessages = remap.getMidiMessages(cmd, channel, param1, param2);
         for (let msg of outMessages) {
             midiOut.send(msg);
         }
